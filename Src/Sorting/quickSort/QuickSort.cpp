cd472b7965e8144421583fd500fe600c63af2e2e
DucThang18DT <DucThang18DT@gmail.com>
2021-08-23 10:47:35 +0700

    update quicksort.cpp

--- Src/Sorting/quickSort/QuickSort.cpp
+++ Src/Sorting/quickSort/QuickSort.cpp
@@ -2,11 +2,11 @@
 
 int _partition(int *array, int left, int right, Direction direction = Direction::S2L)
 {
-    int _l, _r, pivot;
+    int _l = left, _r = right, pivot = array[right];
     switch (direction)
     {
     case Direction::S2L:
-        _l = left; _r = right; pivot = array[right];
+        // _l = left; _r = right; pivot = array[right];
         while (_l < _r)
         {
             while ((array[_l] < pivot) && (_l < _r)) ++_l;
@@ -19,7 +19,7 @@ int _partition(int *array, int left, int right, Direction direction = Direction:
         if (pivot < array[_l]) _swap(&array[_l], &array[right]);
         return _l;
     case Direction::L2S:
-        _l = left; _r = right; pivot = array[right];
+        // _l = left; _r = right; pivot = array[left];
         while (_l < _r)
         {
             while ((array[_l] >= pivot) && (_l < _r)) ++_l;
